34c3d1e995d2979fdd747fcc1ac524c2313ffd8d
faizan23804 <riazfaizan237@gmail.com>
2025-09-22 23:25:29 +0530

    Mongo Json Push

---
+++ push_data.py
@@ -0,0 +1,65 @@
+import os
+import sys
+import json
+import certifi
+import pandas as pd
+import numpy as np
+import pymongo
+from network_securities.exceptions.exceptions import CustomException
+from network_securities.logging.logger import logging
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MONGO_DB_URL=os.getenv("MONGO_DB_URL")
+print(MONGO_DB_URL)
+
+
+ca = certifi.where()
+
+class NetworkDataExtract():
+    def __init__(self):
+        try:
+            pass
+        except Exception as e:
+            raise CustomException(e, sys)
+        
+    def csv_to_json(self,file_path):
+        try:
+            dataset = pd.read_csv(file_path)
+            dataset.reset_index(drop=True,inplace=True)
+            records=list(json.loads(dataset.T.to_json()).values())
+            return records
+
+        except Exception as e:
+            raise CustomException(e,sys)
+        
+    def insert_data_to_mongodb(self,records,database,collection):
+        try:
+            self.records=records
+            self.database=database
+            self.collection=collection
+
+            self.mongo_client=pymongo.MongoClient(MONGO_DB_URL)
+            self.database = self.mongo_client[self.database]
+
+            self.collection = self.database[self.collection]
+            self.collection.insert_many(self.records)
+
+            return(len(self.records))
+        except Exception as e:
+            raise CustomException(e, sys)
+        
+
+
+if __name__=='__main__':
+    FILE_PATH="Network_data\phisingData.csv"
+    Database="NetworkSecurity"
+    Collection="NetworkDataset"
+    networkobj=NetworkDataExtract()
+    records=networkobj.csv_to_json(file_path=FILE_PATH)
+    #print(records)
+    number_of_records=networkobj.insert_data_to_mongodb(records,Database,Collection)
+    print(f"The Total Number of Records are: {number_of_records}")
+
