ecaa0c8428a0deb17b1936f927ba6c2da3dbb35f
faizan23804 <riazfaizan237@gmail.com>
2025-09-30 23:28:07 +0530

    Data Transform initiate

--- network_securities/constant/training_pipeline/__init__.py
+++ network_securities/constant/training_pipeline/__init__.py
@@ -1,6 +1,7 @@
 
 import os
 import sys
+import numpy as np
 
 """
 Defining common constant variable for training pipeline
@@ -33,3 +34,17 @@ DATA_VALIDATION_VALID_DIR:str="validated"
 DATA_VALIDATION_INVALID_DIR:str="invalid"
 DATA_VALIDATION_DRIFT_REPORT_DIR:str="drift_report"
 DATA_VALIDATION_DRIFT_REPORT_FILE_NAME:str="report.yaml"
+
+"""
+Data Transformation related constants
+"""
+DATA_TRANSFORMATION_DIR_NAME:str="data_transformation"
+DATA_TRANSFORMATION_TRANSFORMED_DIR_NAME:str="transformed"
+DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR:str="transformed_object"
+## KNN IMPUTER TO REPLACE NAN VALUE
+DATA_TRANSFORMATION_IMPUTER_PARAMS:dict={
+    "missing_values": np.nan,
+    "n_neighbors": 3,
+    "weights": "uniform"
+
+} 
